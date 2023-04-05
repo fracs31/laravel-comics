f790e5522bc428a05564684a17cf9e8d8e7339e8
fracs31 <pist.fracs95@gmail.com>
2023-04-05 15:53:52 +0200

    ADD footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,195 @@
+<!-- Footer -->
+<footer class="page-footer">
+    <!-- Container -->
+    <div class="container">
+        <!-- Footer Top -->
+        <div class="footer-top">
+            <!-- Informazioni -->
+            <div class="infos">
+                <!-- Box informazione -->
+                <div class="box-info">
+                    <!-- Sottotitolo -->
+                    <h3 class="box-info__title">DC Comics</h3>
+                    <!-- Lista -->
+                    <ul class="box-info__list">
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Characters
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Comics
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Movies
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            TV
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Games
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Videos
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            News
+                        </li>
+                    </ul>
+                </div>
+                <!-- Box informazione -->
+                <div class="box-info">
+                    <!-- Sottotitolo -->
+                    <h3 class="box-info__title">DC</h3>
+                    <!-- Lista -->
+                    <ul class="box-info__list">
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Terms Of Use
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Privacy policy (New)
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Ad Choices
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Advertising
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Jobs
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Subscriptions
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Talent Workshops
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            CPSC Certificates
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Rating
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Shop Help
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Contact Us
+                        </li>
+                    </ul>
+                </div>
+                <!-- Box informazione -->
+                <div class="box-info">
+                    <!-- Sottotitolo -->
+                    <h3 class="box-info__title">Sites</h3>
+                    <!-- Lista -->
+                    <ul class="box-info__list">
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            DC
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            MAD Magazine
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            DC Kids
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            DC Universe
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            DC Power Visa
+                        </li>
+                    </ul>
+                </div>
+                <!-- Box informazione -->
+                <div class="box-info">
+                    <!-- Sottotitolo -->
+                    <h3 class="box-info__title">Shop</h3>
+                    <!-- Lista -->
+                    <ul class="box-info__list">
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            Shop DC
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="box-info__list-item">
+                            MAD Shop DC Collectibles
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Logo grande -->
+            <div class="big-logo">
+                <!-- Immagine -->
+                <img class="big-logo__img" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Big Logo">
+            </div>
+        </div>
+    </div>
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        <!-- Container -->
+        <div class="container">
+            <!-- Sottoiscrizione -->
+            <div class="subscription">
+                <!-- Bottone -->
+                <button class="sign-up">
+                    Sign-up now!
+                </button>
+                <!-- Seguici -->
+                <div class="follow-us">
+                    <!-- Sottotitolo -->
+                    <h4 class="follow-us__title">Follow Us</h4>
+                    <!-- Lista -->
+                    <ul class="follow-us__list">
+                        <!-- Elemento della lista -->
+                        <li class="follow-us__list-item">
+                            <!-- Immagine -->
+                            <img class="follow-us__list-item--img" src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook">
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="follow-us__list-item">
+                            <!-- Immagine -->
+                            <img class="follow-us__list-item--img" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter">
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="follow-us__list-item">
+                            <!-- Immagine -->
+                            <img class="follow-us__list-item--img" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube">
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="follow-us__list-item">
+                            <!-- Immagine -->
+                            <img class="follow-us__list-item--img" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest">
+                        </li>
+                        <!-- Elemento della lista -->
+                        <li class="follow-us__list-item">
+                            <!-- Immagine -->
+                            <img class="follow-us__list-item--img" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
