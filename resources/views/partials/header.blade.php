5a8e47e176f2988d1175e3248802fba907c8948a
fracs31 <pist.fracs95@gmail.com>
2023-04-05 15:30:58 +0200

    ADD images and START header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<!-- Header -->
+<header class="main-header">
+    <!-- Container -->
+    <div class="container">
+        <!-- Navbar -->
+        <nav class="navbar">
+            <!-- Logo -->
+            <div class="navbar__logo">
+                <!-- Immagine -->
+                <img class="navbar__logo--img" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+            </div>
+            <!-- Lista -->
+            <ul class="navbar__list">
+                <!-- Elemento della lista -->
+                <li class="navbar__list-item">
+                    Characters
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
